a385c9794a9a0c099e9c3b7a3d4c64c069292657
Luca Ippolito <luca.ippolito@orbyta.it>
2020-08-16 11:37:34 +0200

    Singly linked list completed with test || reorganization of folders

---
+++ src/js/data_structures/singlyLinkedList.js
@@ -0,0 +1,134 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length= 0;
+    }
+
+    push = (val) => {
+        let n = new Node(val);
+        if(!this.head){
+            this.head = n;
+            this.tail = n;
+        } else {
+            this.tail.next = n;
+            this.tail = n;
+        }
+        this.length ++;
+        return this;
+    }
+
+    pop = () => {
+        if(!this.head) return undefined;
+        let lastNode = this.head;
+        let currentNode = lastNode;
+        while(currentNode.next){
+            lastNode = currentNode;
+            currentNode = currentNode.next;
+        } 
+        this.tail=lastNode;
+        this.tail.next=null;
+        this.length --;
+        if (this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return currentNode;
+    }
+    
+    shift = () => {
+        if(!this.head) return undefined;
+        let n = this.head;
+        this.head = n.next;
+        this.length--;
+        if (this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return n;
+    }
+
+    unshift = (val) => {
+        let n = new Node(val);
+        if(!this.head){
+            this.head = n;
+            this.tail = n;
+        } else {
+            n.next = this.head;
+            this.head = n;
+        }
+        this.length++;
+        return this;
+    }
+
+    get = (index) => {
+        if (index >= this.length || index < 0 || isNaN(index)){
+            return null;
+        }
+        let n = this.head;
+        for(let i=0;i<index;i++){
+            n = n.next;
+        }
+        return n;
+    }
+
+ /*
+   Set: change the value of a node based on index
+ */
+
+    set = (index,val) => {
+        let n = this.get(index);
+        if (n) {
+            n.val = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert = (index,val) => {
+        if (index < 0 || index > this.length || isNaN(index)) return false;
+        if (index === this.length) return !!this.push(val); 
+        if (index === 0) return !!this.unshift(val);
+
+        let n = new Node(val);
+        let previous = this.get(index-1);
+        n.next = previous.next;
+        previous.next = n;
+        this.length++;
+        return true;
+    }
+
+    remove = (index) => {
+        if (index < 0 || index >= this.length || isNaN(index)) return null;
+        if (index === this.length-1) return this.pop();
+        if (index === 0) return this.shift();
+        let previous = this.get(index-1);
+        let removed = previous.next;
+        previous.next = removed.next;
+        this.length--;
+        return removed;
+    }
+
+    reverse = () => {
+        if(this.length === 0 || this.length === 1) return this;
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        let prev = null;
+        let next;
+        for(let i = 0; i<this.length; i++){
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
+        }
+        return this;
+    }
+}
---
+++ src/test/data_structures/singlyLinkedList.test.js
@@ -0,0 +1,365 @@
+describe('Singly Linked List:', function () {
+    
+    it('test Node', function () {
+        var first = new Node("Test")
+        first.next = new Node("success")
+        expect(first.next.val).toEqual("success");
+    });
+
+    it('Initialization - values start at null', function () {
+        const list = new SinglyLinkedList;
+        expect(list.head).toBe(null);
+        expect(list.tail).toBe(null);
+        expect(list.length).toBe(0);
+    });
+
+    it('Push one element', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        expect(list.head.val).toBe(3);
+        expect(list.tail.val).toBe(3);
+        expect(list.length).toBe(1);
+    });
+
+    it('Push three element', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.head.val).toBe(3);
+        expect(list.tail.val).toBe(9);
+        expect(list.length).toBe(3);
+    });
+
+    it('Pop the only element', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        expect(list.pop().val).toBe(3);
+        expect(list.head).toBe(null);
+        expect(list.tail).toBe(null);
+        expect(list.length).toBe(0);
+    });
+
+    it('Pop one element', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.pop().val).toBe(9);
+        expect(list.head.val).toBe(3);
+        expect(list.tail.val).toBe(6);
+        expect(list.length).toBe(2);
+    });
+
+    it('Pop two element', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.pop().val).toBe(9);
+        expect(list.pop().val).toBe(6);
+        expect(list.head.val).toBe(3);
+        expect(list.tail.val).toBe(3);
+        expect(list.length).toBe(1);
+    });
+
+    it('Shift the only element', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        expect(list.shift().val).toBe(3);
+        expect(list.head).toBe(null);
+        expect(list.tail).toBe(null);
+        expect(list.length).toBe(0);
+    });
+
+    it('Shift one element', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.shift().val).toBe(3);
+        expect(list.head.val).toBe(6);
+        expect(list.tail.val).toBe(9);
+        expect(list.length).toBe(2);
+    });
+
+    it('Shift two element', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.shift().val).toBe(3);
+        expect(list.shift().val).toBe(6);
+        expect(list.head.val).toBe(9);
+        expect(list.tail.val).toBe(9);
+        expect(list.length).toBe(1);
+    });
+
+    it('Unshift from null', function () {
+        const list = new SinglyLinkedList;
+        list.unshift(3);
+        expect(list.head.val).toBe(3);
+        expect(list.tail.val).toBe(3);
+        expect(list.length).toBe(1);
+    });
+
+    
+    it('Unshift three element', function () {
+        const list = new SinglyLinkedList;
+        list.unshift(3);
+        list.unshift(6);
+        list.unshift(9);
+        expect(list.head.val).toBe(9);
+        expect(list.tail.val).toBe(3);
+        expect(list.length).toBe(3);
+    });
+
+    it('get unexistent positive index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.get(3)).toBe(null);
+    });
+
+    it('get unexistent negative index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.get("dasd")).toBe(null);
+    });
+
+    it('get index 0', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.get(0).val).toBe(3);
+    });
+
+    it('get index 1', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.get(1).val).toBe(6);
+    });
+
+    it('get last index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.get(2).val).toBe(9);
+    });
+
+    it('set unexistent positive index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.set(3)).toBe(false);
+    });
+
+    it('set unexistent string index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.set("dasd")).toBe(false);
+    });
+
+    it('set second item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.set(1,12)).toBe(true);
+        expect(list.head.next.val).toBe(12);
+    });
+
+    it('set third item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        list.push(10);
+        expect(list.set(2,12)).toBe(true);
+        expect(list.get(1).next.val).toBe(12);
+        expect(list.length).toBe(4);
+    });
+
+    it('insert unexistent negative index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.set("dasd")).toBe(false);
+        expect(list.length).toBe(3);
+    });
+
+    it('insert unexistent string index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.set("dasd")).toBe(false);
+        expect(list.length).toBe(3);
+    });
+
+    it('insert first item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.insert(0,12)).toBe(true);
+        expect(list.head.val).toBe(12);
+        expect(list.length).toBe(4);
+    });
+    it('insert second item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.insert(1,12)).toBe(true);
+        expect(list.head.next.val).toBe(12);
+        expect(list.length).toBe(4);
+    });
+
+    it('insert third item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        list.push(10);
+        expect(list.insert(2,12)).toBe(true);
+        expect(list.get(1).next.val).toBe(12);
+        expect(list.length).toBe(5);
+    });
+
+    it('insert last item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.insert(3,12)).toBe(true);
+        expect(list.tail.val).toBe(12);
+        expect(list.tail.next).toBe(null);
+        expect(list.length).toBe(4);
+    });
+
+    it('remove unexistent string index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.remove("dasd")).toBe(null);
+        expect(list.length).toBe(3);
+    });
+
+    it('remove unexistent negative index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.remove("dasd")).toBe(null);
+        expect(list.length).toBe(3);
+    });
+
+    it('remove unexistent positive index', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.remove(3)).toBe(null);
+        expect(list.length).toBe(3);
+    });
+
+    it('remove the only one item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        expect(list.remove(0).val).toBe(3);
+        expect(list.head).toBe(null);
+        expect(list.length).toBe(0);
+    });
+
+    it('remove first item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.remove(0).val).toBe(3);
+        expect(list.head.val).toBe(6);
+        expect(list.length).toBe(2);
+    });
+
+    it('remove second item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.remove(1).val).toBe(6);
+        expect(list.head.next.val).toBe(9);
+        expect(list.length).toBe(2);
+    });
+
+    it('remove third item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        list.push(10);
+        expect(list.remove(2).val).toBe(9);
+        expect(list.get(1).next.val).toBe(10);
+        expect(list.length).toBe(3);
+    });
+
+    it('remove last item', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        expect(list.remove(2).val).toBe(9);
+        expect(list.tail.val).toBe(6);
+        expect(list.length).toBe(2);
+    });
+
+    it('reverse right', function () {
+        const list = new SinglyLinkedList;
+        list.push(3);
+        list.push(6);
+        list.push(9);
+        list.push(12);
+        list.push(15);
+        list.reverse();
+        expect(list.head.val).toBe(15);
+        expect(list.head.next.val).toBe(12);
+        expect(list.head.next.next.val).toBe(9);
+        expect(list.head.next.next.next.val).toBe(6);
+        expect(list.head.next.next.next.next.val).toBe(3);
+        expect(list.tail.val).toBe(3);
+        expect(list.tail.next).toBe(null);
+        expect(list.length).toBe(5);
+    });
+
+    it('reverse one item', function () {
+        const list = new SinglyLinkedList;
+        list.push(6);
+        list.reverse();
+        expect(list.head.val).toBe(6);
+        expect(list.head.next).toBe(null);
+        expect(list.tail.val).toBe(6);
+        expect(list.tail.next).toBe(null);
+        expect(list.length).toBe(1);
+    });
+
+    it('Reverse nothing', function () {
+        const list = new SinglyLinkedList;
+        list.reverse()
+        expect(list.head).toBe(null);
+        expect(list.tail).toBe(null);
+        expect(list.length).toBe(0);
+    });
+
+});
